377cbd87276c93ba302e9afdad7b8c626e9a96dd
Gustavo Gonzalez <gadol.gonzalez@gmail.com>
2020-04-19 22:04:56 -0400

    done some exercises on basic tactics. still 2 sorrys

--- lean/love02_backward_proofs_exercise_sheet.lean
+++ lean/love02_backward_proofs_exercise_sheet.lean
@@ -1,3 +1,4 @@
+
 import .love02_backward_proofs_demo
 
 
@@ -18,35 +19,57 @@ Section 2.3 in the Hitchhiker's Guide. -/
 
 lemma I (a : Prop) :
   a → a :=
-sorry
+begin
+  intro ha,
+  assumption,
+end
 
 lemma K (a b : Prop) :
   a → b → b :=
-sorry
+begin
+  intros ha hb,
+  assumption,
+end
 
 lemma C (a b c : Prop) :
   (a → b → c) → b → a → c :=
-sorry
+begin
+  intros f hb ha,
+  exact f ha hb,
+end
 
 lemma proj_1st (a : Prop) :
   a → a → a :=
-sorry
+begin
+  intros ha₁ ha₂,
+  exact ha₁,
+end
 
 /-! Please give a different answer than for `proj_1st`: -/
 
 lemma proj_2nd (a : Prop) :
   a → a → a :=
-sorry
+begin
+  intros ha₁ ha₂,
+  exact ha₂,
+end
 
 lemma some_nonsense (a b c : Prop) :
   (a → b → c) → a → (a → c) → b → c :=
-sorry
+begin
+  intros f ha g hb,
+  exact f ha hb,
+end
 
 /-! 1.2. Prove the contraposition rule using basic tactics. -/
 
 lemma contrapositive (a b : Prop) :
   (a → b) → ¬ b → ¬ a :=
-sorry
+begin
+  intros f hnb hna,
+  apply hnb,
+  exact f hna,
+end
 
 /-! 1.3. Prove the distributivity of `∀` over `∧` using basic tactics.
 
@@ -56,7 +79,22 @@ necessary. -/
 
 lemma forall_and {α : Type} (p q : α → Prop) :
   (∀x, p x ∧ q x) ↔ (∀x, p x) ∧ (∀x, q x) :=
-sorry
+begin
+  split,
+  intro h1,
+  split,
+  intro hx,
+  apply (h1 hx).left,
+
+  intro hx,
+  apply (h1 hx).right,
+
+  intro h2,
+  intro hx,
+  split,
+  exact h2.left hx,
+  exact h2.right hx,
+end
 
 
 /-! ## Question 2: Natural Numbers
@@ -68,22 +106,45 @@ sorry
 
 lemma mul_zero (n : ℕ) :
   mul 0 n = 0 :=
-sorry
+begin
+  induction n,
+  refl,
+  simp [add,mul,n_ih],
+end
+
 
 lemma mul_succ (m n : ℕ) :
   mul (nat.succ m) n = add (mul m n) n :=
-sorry
+begin
+  induction n with d hd,
+  refl,
+  simp [add,mul, hd],
+  sorry,
+end
 
 /-! 2.2. Prove commutativity and associativity of multiplication using the
 `induction` tactic. Choose the induction variable carefully. -/
 
 lemma mul_comm (m n : ℕ) :
   mul m n = mul n m :=
-sorry
+begin
+  induction n with d hd,
+  rw mul_zero,
+  refl,
+  simp [add,mul,hd],
+  rw mul_succ,
+  rw add_comm,
+end
 
 lemma mul_assoc (l m n : ℕ) :
-  mul (mul l m) n = mul l (mul m n) := :=
-sorry
+  mul (mul l m) n = mul l (mul m n) := 
+begin
+  induction n with d hd,
+  refl,
+  simp [add,mul],
+  rw mul_add,
+  rw hd,
+end
 
 /-! 2.3. Prove the symmetric variant of `mul_add` using `rewrite`. To apply
 commutativity at a specific position, instantiate the rule by passing some
@@ -91,7 +152,10 @@ arguments (e.g., `mul_comm _ l`). -/
 
 lemma add_mul (l m n : ℕ) :
   mul (add l m) n = add (mul n l) (mul n m) :=
-sorry
+begin
+  rw mul_comm,
+  rw mul_add,
+end
 
 
 /-! ## Question 3 (**optional**): Intuitionistic Logic
@@ -121,13 +185,34 @@ and `peirce`. -/
 
 lemma peirce_of_em :
   excluded_middle → peirce :=
-sorry
+begin
+  simp [excluded_middle, peirce],
+  intro em,
+  intros a b,
+  intro f,
+  apply or.elim (em a),
+  intro ha,
+  assumption,
+
+  intro hna,
+  cc,
+end
+
+--#print peirce_of_em
 
 /-! 3.2 (**optional**). Prove the following implication using tactics. -/
 
 lemma dn_of_peirce :
   peirce → double_negation :=
-sorry
+begin
+  simp [peirce, double_negation],
+  intro p,
+  intro a,
+  intro hnna,
+  cc,
+end
+
+--#print dn_of_peirce
 
 /-! We leave the missing implication for the homework: -/
 
@@ -135,7 +220,14 @@ namespace sorry_lemmas
 
 lemma em_of_dn :
   double_negation → excluded_middle :=
-sorry
+begin
+  simp [double_negation,excluded_middle],
+  intro dn,
+  intro a,
+  have dna, from dn a,
+  sorry,
+  
+end
 
 end sorry_lemmas
 
